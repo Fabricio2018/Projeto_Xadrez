1ab8cd324d717251b7ca25313636d4451a765f0f
Fabrício Dias <fabricio.dias102@gmail.com>
2021-04-17 14:55:38 -0300

    Exercicio 2 sobre Herança e Polimorfismo, com base em uma lista de produtos.

---
+++ Exerc_HP_2/Entities/ImportedProduct.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Exerc_HP_2.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double  customsFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customsFee)
+            : base(name, price)
+        {
+            this.customsFee = customsFee;
+        }
+
+        public double totalPrice()
+        {
+            return Price + customsFee;
+        }
+
+        public override string priceTag()
+        {
+            return Name
+                + " $ "
+                + totalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + customsFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+    }
+}
