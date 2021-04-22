62bac865b6b268c87027bf64fdf28a70cf64e5ea
Fabrício Oliveira Dias <fabricio.dias102@gmail.com>
2021-04-22 16:41:50 -0300

    Começando a colocar peças no tabuleiro

--- Projeto_Xadrez/Tabuleiro/Tabuleiro.cs
+++ Projeto_Xadrez/Tabuleiro/Tabuleiro.cs
@@ -1,24 +1,23 @@
-﻿using System;
+﻿namespace tabuleiro {
+    class Tabuleiro {
 
-namespace tabuleiro
-{
-    class Tabuleiro
-    {
         public int linhas { get; set; }
         public int colunas { get; set; }
-
         private Peca[,] pecas;
 
-        public Tabuleiro(int linhas, int colunas)
-        {
+        public Tabuleiro(int linhas, int colunas) {
             this.linhas = linhas;
             this.colunas = colunas;
-            pecas = new Peca[linhas, colunas];
+            pecas = new Peca[linhas, colunas]; 
         }
 
-        public Peca peca(int linha, int coluna)
-        {
+        public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos) {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
         }
     }
 }
