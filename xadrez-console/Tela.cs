c27f1bc7afc3a90ec785378277f43eb99a69f2ab
Fabrício Oliveira Dias <fabricio.dias102@gmail.com>
2021-04-23 14:58:45 -0300

    Tratando erros com excecao personalizada

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using tabuleiro;
 
-namespace xadrez_console {
-    class Tela {
+namespace xadrez_console
+{
+    class Tela
+    {
 
-        public static void imprimirTabuleiro(Tabuleiro tab) {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
 
-            for (int i=0; i<tab.linhas; i++) {
-                for (int j=0; j<tab.colunas; j++) {
-                    if (tab.peca(i, j) == null) {
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
                         Console.Write("- ");
                     }
-                    else {
+                    else
+                    {
                         Console.Write(tab.peca(i, j) + " ");
                     }
                 }
--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -1,12 +1,16 @@
 ﻿using tabuleiro;
 
-namespace xadrez {
-    class Rei : Peca {
+namespace xadrez
+{
+    class Rei : Peca
+    {
 
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor) {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
         }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return "R";
         }
     }
